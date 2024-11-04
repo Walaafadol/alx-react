c6b42a481b28aa69ecc65f186429171d77ca18d2
Walaafadol <walaafadul7@gmail.com>
2024-11-04 20:29:38 +0000

    comm

---
+++ 0x02-react_props/task_4/dashboard/src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <>
+	  <div className="App-footer">
+        <p>Copyright 2020 - Holberton School</p>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
